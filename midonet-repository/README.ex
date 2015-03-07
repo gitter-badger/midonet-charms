a0977c547f0e6005251a93436a86507465d291dd
Antoni Segura Puimedon <toni@midokura.com>
2015-03-07 02:52:15 +0100

    WorkInProgress: Add midonet-repository charm
    This charm will install the necessary midonet repositories.  There
    should be another charm that installs MEM and that if they are related
    they set up the proper priorities.
    
    The current charm could probably be improved by only installing the
    repos when a relation joins that needs a specific repository. For
    example, it would only install the neutron plugin repo if the nova
    compute relation joins.
    
    WIP!!! Needs testing and refining.
    
    Change-Id: Ieec4de81dae57ee1c335e02fd6e07219dd2f8faf
    Signed-off-by: Antoni Segura Puimedon <toni@midokura.com>

---
+++ midonet-repository/README.ex
@@ -0,0 +1,38 @@
+Copyright (c) 2015 Midokura SARL, All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+# Overview
+
+This charm provides midonet-repository settings required for deploying midonet
+services and its OpenStack integration.
+
+It also installs puppet and the midonet-midonet module for it that is used by
+the midonet charms for both installation and configuring.
+
+
+# Usage
+
+   juju deploy midonet-repository
+
+
+# Contact Information
+
+juju-charms at https://slack.midonet.org
+
+## MidoNet
+
+- Website: https://midonet.org
+- Bug tracker: https://midonet.atlassian.net
+- Mailing list: https://lists.midonet.org/listinfo
